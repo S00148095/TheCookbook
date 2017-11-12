2bab8f1a375c1c962ac7121f54935fd7f9aee129
silentgolem <liamlogan16@yahoo.co.uk>
2017-11-12 18:33:20 +0000

    Recipe styling added

--- cookbook/src/app/home/home.component.ts
+++ cookbook/src/app/home/home.component.ts
@@ -9,8 +9,8 @@ import { Recipe } from '../recipe';
 export class HomeComponent implements OnInit {
 
   Recipes:Recipe[]=[
-    new Recipe("Beans on Toast",5,0,"http://i263.photobucket.com/albums/ii151/daydrifter26/Toast-2.jpg",["Bread","Beans"],["Put bread in toaster at appropriate settings","Put beans in microwave","Wait for the toast and beans to be done","Pour beans on top of the toast","Serve"]),
-    new Recipe("Toast",5,0,"http://i263.photobucket.com/albums/ii151/daydrifter26/Toast-2.jpg",["Bread"],["Put bread in toaster at appropriate settings","Wait for the toast to be done","Serve"])
+    new Recipe("Beans on Toast",5,0,"https://i.imgur.com/GmNT8tF.jpg",["Bread","Beans"],["Put bread in toaster at appropriate settings","Put beans in microwave","Wait for the toast and beans to be done","Pour beans on top of the toast","Serve"]),
+    new Recipe("Toast",5,0,"https://i.imgur.com/sUTxDOn.jpg",["Bread"],["Put bread in toaster at appropriate settings","Wait for the toast to be done","Serve"])
   ]
   constructor() { }
 
