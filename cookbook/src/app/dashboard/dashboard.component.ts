76fad200ec149468a0fdfbefb35341e3b77d4725
PsychoHacks <jamie.v732@gmail.com>
2017-11-30 22:21:52 +0000

    Still broken

--- cookbook/src/app/dashboard/dashboard.component.ts
+++ cookbook/src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,23 @@
 import { StorageService } from '../services/storage.service';
 import { Observable } from 'rxjs/Rx';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../User';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireList } from 'angularfire2/database/interfaces';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-  userInfo: Observable<any[]>;
+export class DashboardComponent implements OnInit {
+  public userList: AngularFireList<User[]>;
+  public uid: string;
 
-  constructor(private db: AngularFireDatabase, private service: StorageService) {
-    let userInfo = this.service.GetUserInfo();
-  }
+  constructor(private service: StorageService) { }
 
   ngOnInit() {
+    this.userList = this.service.GetUserList({ orderByChild: 'UserID', equalTo: this.service.uid })
+    console.log(this.userList);
   }
 }
--- cookbook/src/app/services/storage.service.ts
+++ cookbook/src/app/services/storage.service.ts
@@ -1,30 +1,44 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { User } from '../User';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Injectable()
 export class StorageService {
     public User: User;
-    public uid: string;
-    public userInfo: AngularFireList<any[]>;
-    public user: any[];
+    public uid: string = '';
     url: string = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/';
-    
-   constructor(private afd: AngularFireDatabase, private afa: AngularFireAuth, private http: HttpClient, public afs: AngularFirestore) {
-        this.uid = "";
-        this.userInfo = afd.list('users');
-        //this.userInfo.valueChanges().subscribe(res => res.where('UserID', '==', this.uid));
-     }
+
+    public userList: AngularFireList<User[]>;
+    public userInfo: AngularFireObject<User>;
+    private basePath: string = '/users';
+
+    constructor(private db: AngularFireDatabase, private afa: AngularFireAuth, private http: HttpClient) {
+    }
+
+    GetUserList(query = {}):AngularFireList<User[]> {
+        this.userList = this.db.list(this.basePath ,
+            query => query
+        );
+        return this.userList
+    }
+
+    GetUserInfo(key: string): AngularFireObject<User>{
+        const userPath = '${this.basePath}/${uid}';
+        this.userInfo = this.db.object(userPath)
+        return this.userInfo
+    }
+
     GetUser(): User {
         if (this.uid != null) {
             //this.User = this.afd.object('/' + this.uid).map()
         }
         return this.User
     }
+
     GetUID() {
         this.afa.authState.subscribe((resp) => {
             if (resp != null) {
@@ -34,19 +48,19 @@ export class StorageService {
             }
         });
     }
-    GetUserInfo() {
-        let userInfo = this.afs.collection('the-cookbook', ref => ref.where('UserID', '==', this.uid))
-    }
+
     sendGetRequestRandomRecipes(): Observable<any> {
         return this.http.get(this.url + "recipes/" + "random?limitLicense=true&number=10", {
             headers: new HttpHeaders().set('X-Mashape-Key', 'tM5qhvbLgOmshXF6C08zcPSGG80vp1z3sj9jsnF0zNHLYcu6A8'),
         });
     }
+
     sendGetRequestRecipeByID(ID): Observable<any> {
         return this.http.get(this.url + "recipes/" + ID + "/information?includeNutrition=false", {
             headers: new HttpHeaders().set('X-Mashape-Key', 'tM5qhvbLgOmshXF6C08zcPSGG80vp1z3sj9jsnF0zNHLYcu6A8'),
         });
     }
+
     sendGetRequestAutocomplete(value): Observable<any> {
         console.log(this.url + "food/ingredients/autocomplete?metaInformation=false&number=10&query=" + value);
         return this.http.get(this.url + "food/ingredients/autocomplete?metaInformation=false&number=10&query=" + value, {
