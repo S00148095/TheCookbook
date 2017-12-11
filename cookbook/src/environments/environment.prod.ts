abea976e34f924d3d87ecdca91cbfa5acce44150
silentgolem <liamlogan16@yahoo.co.uk>
2017-12-11 18:46:30 +0000

    1.0.1 broken

--- cookbook/src/environments/environment.prod.ts
+++ cookbook/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDZFTDf5-U8rJqWhMKwJXxe_zMz12peZLw",
+    authDomain: "the-cookbook.firebaseapp.com",
+    databaseURL: "https://the-cookbook.firebaseio.com",
+    projectId: "the-cookbook",
+    storageBucket: "",
+    messagingSenderId: "1043942864683"
+  }
 };
